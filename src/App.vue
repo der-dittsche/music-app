40b968418e438a79c475a7f08c0beaaefcd2f38f
Sascha Dietrich <sascha_dietrich@outlook.com>
2023-02-20 22:52:19 +0100

    add eslinter rules, firebase dep

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,29 @@
     <nav>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/auth">Auth</RouterLink>
     </nav>
   </header>
 
   <RouterView />
 </template>
 
-<script setup>
+<script>
 import { RouterLink, RouterView } from "vue-router";
+import { useUserStore } from "./stores/users";
+
+export default {
+  components: {
+    RouterLink,
+    RouterView,
+  },
+  data() {
+    const storeUser = useUserStore();
+    return {
+      storeUser,
+    };
+  },
+};
 </script>
 
 <style scoped></style>
