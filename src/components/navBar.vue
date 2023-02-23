0d96c8712703ba8f76fb6da926732a0627d04560
Sascha Dietrich <sascha_dietrich@outlook.com>
2023-02-23 21:18:43 +0100

    feat: refactor navbar

---
+++ src/components/navBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav>
+    <div class="nav__title">DNW</div>
+    <ul class="primary-nav">
+      <li>
+        <RouterLink to="/"><div class="nav-item">Home</div></RouterLink>
+      </li>
+      <li v-if="storeUser.user.id">
+        <RouterLink to="/about"><div class="nav-item">About</div></RouterLink>
+      </li>
+      <li v-if="storeUser.user.id">
+        <RouterLink to="/manage"><div class="nav-item">Manage</div></RouterLink>
+      </li>
+    </ul>
+    <div class="nav__auth">
+      <RouterLink to="/auth"
+        ><div class="nav-item" v-if="!storeUser.user.id">Auth</div></RouterLink
+      >
+      <div
+        class="nav-item"
+        @click="storeUser.logoutUser"
+        v-if="storeUser.user.id"
+      >
+        LogOut
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { useUserStore } from "@/stores/users";
+
+export default {
+  data() {
+    const storeUser = useUserStore();
+    return {
+      storeUser,
+    };
+  },
+};
+</script>
