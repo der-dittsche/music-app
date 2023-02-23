ca9b47e5c839eef874d9374067def082c574a8e8
Sascha Dietrich <sascha_dietrich@outlook.com>
2023-02-23 21:20:09 +0100

    feat: add player component

---
+++ src/components/songControll.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="player__controll">
+    <div class="button">play</div>
+    <div class="time-start">00:00</div>
+    <div class="time-bar">
+      <div class="time-bar-line">
+        <div class="time-bar-line-progress"></div>
+      </div>
+    </div>
+    <div class="time-end">04:17</div>
+  </div>
+</template>
