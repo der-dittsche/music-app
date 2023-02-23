7eb9d77aac1dedeca49dc5ee46c0e018a1415972
Sascha Dietrich <sascha_dietrich@outlook.com>
2023-02-23 21:18:15 +0100

    feat: create store for songs

---
+++ src/stores/songs.js
@@ -0,0 +1,80 @@
+import { defineStore } from "pinia";
+import { db } from "@/stores/firebase";
+import {
+  collection,
+  onSnapshot,
+  addDoc,
+  doc,
+  updateDoc,
+  deleteDoc,
+  query,
+} from "firebase/firestore";
+
+let songsCollectionRef = collection(db, "songs");
+let songsCollectionQuerry = query(songsCollectionRef);
+let getSongsSnapshot = null;
+
+export const useSongsStore = defineStore("storeSongs", {
+  state: () => {
+    return {
+      songs: [],
+      songsLoaded: false,
+    };
+  },
+  actions: {
+    async getSongs() {
+      this.songsLoaded = false;
+      getSongsSnapshot = onSnapshot(songsCollectionQuerry, (querySnapshot) => {
+        const songs = [];
+        querySnapshot.forEach((doc) => {
+          const song = {
+            id: doc.id,
+            comment_count: doc.data().comment_count,
+            display_name: doc.data().display_name,
+            original_name: doc.data().original_name,
+            modified_name: doc.data().modified_name,
+            artist: doc.data().artist,
+            genre: doc.data().genre,
+            url: doc.data().url,
+            userid: doc.data().userid,
+          };
+          songs.push(song);
+        });
+
+        this.songs = songs;
+        this.songsLoaded = true;
+      });
+    },
+    async addSong(song) {
+      songsCollectionRef = collection(db, "songs");
+      await addDoc(songsCollectionRef, {
+        userid: song.userid,
+        comment_count: song.comment_count,
+        display_name: song.display_name,
+        original_name: song.original_name,
+        modified_name: song.modified_name,
+        artist: song.artist,
+        genre: song.genre,
+        url: song.url,
+      });
+    },
+    async updateSong(song) {
+      songsCollectionRef = collection(db, "songs");
+      await updateDoc(doc(songsCollectionRef, song.id), {
+        display_name: song.display_name,
+        original_name: song.original_name,
+        modified_name: song.modified_name,
+        artist: song.artist,
+        genre: song.genre,
+      });
+    },
+    async deleteSongs(songtodelete) {
+      songsCollectionRef = collection(db, "songs");
+      await deleteDoc(doc(songsCollectionRef, songtodelete));
+    },
+    clearSongs() {
+      this.songs = [];
+      if (getSongsSnapshot) getSongsSnapshot(); // unsubscribe from any listener
+    },
+  },
+});
